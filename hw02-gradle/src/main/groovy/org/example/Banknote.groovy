1c2cb1225bdf30d83ceb47215dd8d61c6315f838
fermsk <104899352+fermsk@users.noreply.github.com>
2025-09-15 08:49:15 +0300

    Update Banknote.groovy

--- hw02-gradle/src/main/groovy/org/example/Banknote.groovy
+++ hw02-gradle/src/main/groovy/org/example/Banknote.groovy
@@ -1,1 +1,22 @@
-
+class Banknote implements Comparable<Banknote> {
+    final int denomination
+    
+    Banknote(int denomination) {
+        this.denomination = denomination
+    }
+    
+    @Override
+    int compareTo(Banknote other) {
+        return other.denomination <=> this.denomination // Descending order
+    }
+    
+    @Override
+    String toString() {
+        return "₽${denomination}"
+    }
+    
+    // Operator overloading for addition
+    def plus(Banknote other) {
+        return this.denomination + other.denomination
+    }
+}
