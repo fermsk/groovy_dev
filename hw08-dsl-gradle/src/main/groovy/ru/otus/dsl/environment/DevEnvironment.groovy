b8930c94b3613cc397441f84d0265ebccabd43d3
fermsk <104899352+fermsk@users.noreply.github.com>
2025-10-21 08:01:24 +0300

    Update DevEnvironment.groovy

--- hw08-dsl-gradle/src/main/groovy/ru/otus/dsl/environment/DevEnvironment.groovy
+++ hw08-dsl-gradle/src/main/groovy/ru/otus/dsl/environment/DevEnvironment.groovy
@@ -1,1 +1,17 @@
+package ru.otus.dsl.environment
 
+class DevEnvironment extends BaseEnvironment {
+    @Override
+    void configureEnvironment() {
+        config.with {
+            http {
+                port 8080
+                secure false
+            }
+            https {
+                port 4443
+                secure true
+            }
+        }
+    }
+}
