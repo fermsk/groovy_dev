237514940b8ff22755df74c217ca3b74e6f6095a
fermsk <104899352+fermsk@users.noreply.github.com>
2025-10-21 08:03:45 +0300

    Update ConfigurationParser.groovy

--- hw08-dsl-gradle/src/main/groovy/ru/otus/dsl/parser/ConfigurationParser.groovy
+++ hw08-dsl-gradle/src/main/groovy/ru/otus/dsl/parser/ConfigurationParser.groovy
@@ -1,1 +1,13 @@
+package ru.otus.dsl.parser
 
+import ru.otus.dsl.config.ServerConfig
+
+class ConfigurationParser {
+    static ServerConfig parseConfig(@DelegatesTo(ServerConfig) Closure closure) {
+        def config = new ServerConfig()
+        closure.delegate = config
+        closure.resolveStrategy = Closure.DELEGATE_FIRST
+        closure()
+        return config
+    }
+}
