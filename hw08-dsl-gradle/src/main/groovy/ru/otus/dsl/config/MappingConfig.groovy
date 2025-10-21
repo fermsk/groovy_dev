176580717759f797da9255606668dece06dfff72
fermsk <104899352+fermsk@users.noreply.github.com>
2025-10-21 07:59:45 +0300

    Update MappingConfig.groovy

--- hw08-dsl-gradle/src/main/groovy/ru/otus/dsl/config/MappingConfig.groovy
+++ hw08-dsl-gradle/src/main/groovy/ru/otus/dsl/config/MappingConfig.groovy
@@ -1,1 +1,14 @@
+package ru.otus.dsl.config
 
+class MappingConfig {
+    String url
+    boolean active
+    
+    def url(String url) {
+        this.url = url
+    }
+    
+    def active(boolean active) {
+        this.active = active
+    }
+}
