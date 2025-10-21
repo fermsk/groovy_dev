cce8073f669694c9b5f64df0e8e606fe91aac93e
fermsk <104899352+fermsk@users.noreply.github.com>
2025-10-21 08:02:02 +0300

    Update ProdEnvironment.groovy

--- hw08-dsl-gradle/src/main/groovy/ru/otus/dsl/environment/ProdEnvironment.groovy
+++ hw08-dsl-gradle/src/main/groovy/ru/otus/dsl/environment/ProdEnvironment.groovy
@@ -1,1 +1,17 @@
+package ru.otus.dsl.environment
 
+class ProdEnvironment extends BaseEnvironment {
+    @Override
+    void configureEnvironment() {
+        config.with {
+            http {
+                port 80
+                secure false
+            }
+            https {
+                port 443
+                secure true
+            }
+        }
+    }
+}
