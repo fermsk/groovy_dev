acf0fa988f776f6b3e518e6ce5c63a89fb010171
fermsk <104899352+fermsk@users.noreply.github.com>
2025-10-21 08:04:51 +0300

    Update ConfigurationExample.groovy

--- hw08-dsl-gradle/src/main/groovy/ru/otus/dsl/ConfigurationExample.groovy
+++ hw08-dsl-gradle/src/main/groovy/ru/otus/dsl/ConfigurationExample.groovy
@@ -1,1 +1,53 @@
+package ru.otus.dsl
 
+import ru.otus.dsl.environment.DevEnvironment
+import ru.otus.dsl.environment.ProdEnvironment
+import ru.otus.dsl.environment.TestEnvironment
+import ru.otus.dsl.parser.ConfigurationParser
+
+// Base configuration example
+def config = ConfigurationParser.parseConfig {
+    name "MyTest"
+    description "Apache Tomcat"
+    
+    http {
+        port 8080
+        secure false
+    }
+    
+    https {
+        port 4443
+        secure true
+    }
+    
+    mapping {
+        url "/"
+        active true
+    }
+    mapping {
+        url "/login"
+        active false
+    }
+}
+
+// Environment-specific configurations
+def devConfig = new DevEnvironment()
+devConfig.configureEnvironment()
+
+def prodConfig = new ProdEnvironment()
+prodConfig.configureEnvironment()
+
+def testConfig = new TestEnvironment()
+testConfig.configureEnvironment()
+
+println "Base configuration:"
+println config.dump()
+
+println "\nDev configuration:"
+println devConfig.config.dump()
+
+println "\nProd configuration:"
+println prodConfig.config.dump()
+
+println "\nTest configuration:"
+println testConfig.config.dump()
