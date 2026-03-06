8cb08c78a016f076fca9a2513b85262bc39671fe
fermsk <104899352+fermsk@users.noreply.github.com>
2026-03-06 12:51:07 +0300

    Delete hw03-gradle directory

--- hw03-gradle/src/main/groovy/org/example/Event.groovy
+++
@@ -1,9 +0,0 @@
-class Event {
-    String message
-    LocalDateTime dateTime
-
-    Event(String message, LocalDateTime dateTime) {
-        this.message = message
-        this.dateTime = dateTime
-    }
-}
